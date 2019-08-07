aa9fde666c50cbf3c3f2ec8c3c0ee1cb2c6a23a1
Federico De Martino <federicodm90@gmail.com>
2019-08-07 13:59:15 +0200

    exercise 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'LiuLin',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '04 73 22 97 51',
+    category:     'chinese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '02 782 15 32',
+    category:     'french',
+  },
+  {
+    name:         'Pasta della Nonna',
+    address:      'ixelles, Bruxelles',
+    phone_number: '02 782 15 32',
+    category:     'italian',
+  },
+  {
+    name:         'Les Frittes',
+    address:      'Etterbeek, Brussels',
+    phone_number: '02 782 42 11',
+    category:     'belgian',
+  },
+  {
+    name:         'sushi non',
+    address:      'Evere, Brussels',
+    phone_number: '02 495 32 68',
+    category:     'japanese',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
